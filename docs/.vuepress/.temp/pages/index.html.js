902eeea84f92ddfaccbaf11492fab4be569fc2cc
xiao <944304707@qq.com>
2023-11-12 21:18:56 +0800

    打包

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-8daa1a0e\",\"path\":\"/\",\"title\":\"页面的标题\",\"lang\":\"zh-CN\",\"frontmatter\":{\"lang\":\"zh-CN\",\"title\":\"页面的标题\",\"description\":\"页面的描述\"},\"headers\":[{\"level\":2,\"title\":\"你好世界\",\"slug\":\"你好世界\",\"link\":\"#你好世界\",\"children\":[]},{\"level\":2,\"title\":\"之间\",\"slug\":\"之间\",\"link\":\"#之间\",\"children\":[{\"level\":3,\"title\":\"你好\",\"slug\":\"你好\",\"link\":\"#你好\",\"children\":[]}]}],\"git\":{\"updatedTime\":1699793937000,\"contributors\":[{\"name\":\"xiao\",\"email\":\"944304707@qq.com\",\"commits\":2}]},\"filePathRelative\":\"README.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
